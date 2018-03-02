2791b218c2b53d09b07429d403a5e79a5712bb0b
Hamahmi <Hamahmi@yahoo.com>
2018-03-02 18:30:35 +0200

    added functional logout
    when pressed it log the user out and redirect to login

--- Front End/src/app/@theme/components/header/header.component.ts
+++ Front End/src/app/@theme/components/header/header.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-
 import { NbMenuService, NbSidebarService } from '@nebular/theme';
-import { NbMenuBag } from '@nebular/theme/components/menu/menu.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-header',
@@ -15,12 +14,12 @@ export class HeaderComponent implements OnInit {
 
   constructor(
     private sidebarService: NbSidebarService,
-    private menuService: NbMenuService
+    private menuService: NbMenuService,
+     private router: Router
   ) {}
 
   ngOnInit() {
-    this.userMenu = [{ title: 'Login' }];
-    this.onMenuItemClick();
+    this.userMenu = [{ title: 'Logout' }];
   }
 
   toggleSidebar(): boolean {
@@ -32,11 +31,11 @@ export class HeaderComponent implements OnInit {
     this.menuService.navigateHome();
   }
 
-  onMenuItemClick() {
-    this.menuService.onItemClick().subscribe((bag: NbMenuBag) => {
-      if (bag.item.title === 'login') {
-        
-      }
-    });
+  onMenuClick(event) {
+    if (event.title === 'Logout') {
+        window.sessionStorage.username = null;
+        alert("You have been logged out !");
+        this.router.navigate(['../dashboard/login']);
+    }
   }
 }
